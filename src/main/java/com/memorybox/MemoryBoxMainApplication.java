6580f4aeb28b1bbc85d6dfea636c51f8cc435d90
kmj <rlaalswh531@gmail.com>
2023-12-15 11:25:25 +0900

    Initial commit

---
+++ src/main/java/com/memorybox/MemoryBoxMainApplication.java
@@ -0,0 +1,13 @@
+package com.memorybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryBoxMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryBoxMainApplication.class, args);
+    }
+
+}
