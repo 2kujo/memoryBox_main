22b16b6890f8e788a6a04dd7551c7fa8cfc7b1d2
Cho-El <68215871+Cho-El@users.noreply.github.com>
2023-12-19 00:44:16 +0900

    feat : CashBox 폴더 구조

---
+++ src/main/java/com/memorybox/api/CashBoxApi.java
@@ -0,0 +1,12 @@
+package com.memorybox.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/cash-boxes")
+@RestController
+public class CashBoxApi {
+
+}
