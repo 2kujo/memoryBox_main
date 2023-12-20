63f76beb1c80f9d5c0b6acec91aac95e59987fdf
김현교 <zkdlajt96@gmail.com>
2023-12-20 17:19:17 +0900

    :bug: fix: Image 기본 생성자 추가

--- src/main/java/com/memorybox/domain/memory/entity/vo/Image.java
+++ src/main/java/com/memorybox/domain/memory/entity/vo/Image.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 public class Image {
     @Column
